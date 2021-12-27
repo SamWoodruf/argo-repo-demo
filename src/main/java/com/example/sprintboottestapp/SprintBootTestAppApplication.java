81c6b8bb6d72383e937878c017c6b6e4918d4a7b
Sam Woodruff <samwoodruff@Sams-MacBook-Pro.local>
2021-12-27 12:34:01 -0600

    first commit

---
+++ src/main/java/com/example/sprintboottestapp/SprintBootTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintboottestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprintboottestapp/SprintBootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintboottestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
